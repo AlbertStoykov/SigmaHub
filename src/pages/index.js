1191636a04770286da6602029c1031f16ff4ca28
Onur <onurbelek@outlook.com>
2022-05-11 11:23:14 +0100

    changed the fetch url in actions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Welcome } from './Welcome';
 export { default as About } from './About';
 export { default as Search } from './Search';
-export { default as NotFound } from './NotFound'
+export { default as NotFound } from './NotFound';
+export { default as Result } from './Result';
