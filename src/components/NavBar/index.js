8f941013f3c5ac2b775ea3885287272773709b53
AlbertStoykov <albert.stoykov@gmail.com>
2022-05-12 11:48:45 +0100

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,10 +7,7 @@ const NavBar = () => {
   return (
     <nav>
       <NavLink className="home-link" to="/">
-        <img
-          src="https://unrivaled-heliotrope-cec12b.netlify.app/images/logo.png"
-          id="logo"
-        />
+        <img src="./logo.png" id="logo" />
       </NavLink>
       <NavLink className="search" to="/search">
         Search
