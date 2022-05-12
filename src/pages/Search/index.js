0a2e8b6f4fd3f15ddb5508462994654c2aeff473
AlbertStoykov <albert.stoykov@gmail.com>
2022-05-12 11:14:13 +0100

    footer

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -8,18 +8,18 @@ function Search() {
   const result = useSelector((state) => state.result);
   const loading = useSelector((state) => state.loading);
   const error = useSelector((state) => state.error);
-  
-  const dispatch = useDispatch();
-//   on load the page gets Eluented
-  useEffect(() => {
-    dispatch(getResult('Eluented'));
-  }, [])
 
-  const search = searchTerm => dispatch(getResult(searchTerm));
-  
+  const dispatch = useDispatch();
+  //   on load the page gets Eluented
+  useEffect(() => {
+    dispatch(getResult("Eluented"));
+  }, []);
+
+  const search = (searchTerm) => dispatch(getResult(searchTerm));
+
   const renderResult = () =>
     loading ? <p>Loading . . .</p> : <UserCard result={result} />;
-    
+
   return (
     <div id="search">
       <p className="what">Which gitHub User do you want to search?</p>
@@ -29,6 +29,24 @@ function Search() {
       ) : (
         renderResult()
       )}
+      <p>
+        © 2022 Repo Tracker. Created by{" "}
+        <a
+          className="footer"
+          href="https://github.com/AlbertStoykov"
+          target="_blank"
+        >
+          Albert
+        </a>{" "}
+        &{" "}
+        <a
+          className="footer"
+          href="https://github.com/Eluented"
+          target="_blank"
+        >
+          Onur
+        </a>
+      </p>
     </div>
   );
 }
